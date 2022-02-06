b2e0f5cf500dcb6f55222c367b19f420a610a161
Alex Mirgorodsky <webster6667g@gmail.com>
2022-02-06 19:59:30 +0300

    disabled cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
-const cors = require('cors')
+// const cors = require('cors')
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5085
 const schema = require('./schema')
 
 const app = express()
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
 app.use('/api', router)
